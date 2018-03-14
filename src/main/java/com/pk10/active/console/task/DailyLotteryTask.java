505bcc74714f7316e02aa8ccf2e73359ba6461f3
rejoiceFromJesus <948870341@qq.com>
2018-03-14 21:50:23 +0800

    optmized DailyLotteryTask

--- src/main/java/com/pk10/active/console/task/DailyLotteryTask.java
+++ src/main/java/com/pk10/active/console/task/DailyLotteryTask.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -19,6 +21,8 @@ import com.pk10.active.console.vo.IssueLotteryVo;
 @Component
 public class DailyLotteryTask {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DailyLotteryTask.class);
+	
 	@Autowired
 	LotteryHistoryService lotteryHistoryService;
 	@Autowired
@@ -27,38 +31,60 @@ public class DailyLotteryTask {
 	//@Scheduled(cron="0/10 * * * * ?")
 	@Scheduled(cron="0 0 1 * * ?")
 	public void execute() {
+		LOGGER.info("DailyLotteryTask starts.....");
+		Long startTime = System.currentTimeMillis();
 		String lastDay = DateTime.now().minusDays(1).toString(Constant.DATE_FORMAT_PATTERN1);
-		String url = "https://www.cp111678.com/data/bjpk10/lotteryList/"+lastDay+".json?"+DateTime.now().getMillis();
-		String result = restTemplate.getForObject(url, String.class);
-		IssueLotteryVo[] issueLotteryVos = JsonUtil.toBean(result, IssueLotteryVo[].class);
-		List<LotteryHistory> list = new ArrayList<LotteryHistory>();
-		for(int i = issueLotteryVos.length-1; i >= 0; i--) {
-			LotteryHistory lotteryHistory = new LotteryHistory();
-			IssueLotteryVo issueLotteryVo = issueLotteryVos[i];
-			String openNum = RejoiceUtil.toString(RejoiceUtil.toString(issueLotteryVo.getOpenNum()));
-			lotteryHistory.setOpenNum(openNum);
-			lotteryHistory.setPeriod(issueLotteryVo.getIssue());
-			lotteryHistory.setOpenDateTime(issueLotteryVo.getOpenDateTime());
-			lotteryHistory.setOpenDate(issueLotteryVo.getOpenDateTime().substring(0, 10));
-			String[] openNums = openNum.split(",");
-			StringBuilder bigSmall = new StringBuilder();
-			StringBuilder oddEven = new StringBuilder();
-			StringBuilder dragonTiger = new StringBuilder();
-			for(int j = 0; j < 10; j++) {
-				oddEven.append(Integer.parseInt(openNums[j]) % 2 == 0? "2": "1").append(",");
-				bigSmall.append(Integer.parseInt(openNums[j]) >= 6 ? "3":"4").append(",");
-				dragonTiger.append(Integer.parseInt(openNums[j]) > Integer.parseInt(openNums[9-j])? "5":"6").append(",");
+		while(true) {
+			try {
+				String url = "https://www.cp111678.com/data/bjpk10/lotteryList/"+lastDay+".json?"+DateTime.now().getMillis();
+				String result = restTemplate.getForObject(url, String.class);
+				IssueLotteryVo[] issueLotteryVos = JsonUtil.toBean(result, IssueLotteryVo[].class);
+				List<LotteryHistory> list = new ArrayList<LotteryHistory>();
+				for(int i = issueLotteryVos.length-1; i >= 0; i--) {
+					LotteryHistory lotteryHistory = new LotteryHistory();
+					IssueLotteryVo issueLotteryVo = issueLotteryVos[i];
+					String openNum = RejoiceUtil.toString(RejoiceUtil.toString(issueLotteryVo.getOpenNum()));
+					lotteryHistory.setOpenNum(openNum);
+					lotteryHistory.setPeriod(issueLotteryVo.getIssue());
+					lotteryHistory.setOpenDateTime(issueLotteryVo.getOpenDateTime());
+					lotteryHistory.setOpenDate(issueLotteryVo.getOpenDateTime().substring(0, 10));
+					String[] openNums = openNum.split(",");
+					StringBuilder bigSmall = new StringBuilder();
+					StringBuilder oddEven = new StringBuilder();
+					StringBuilder dragonTiger = new StringBuilder();
+					for(int j = 0; j < 10; j++) {
+						oddEven.append(Integer.parseInt(openNums[j]) % 2 == 0? "2": "1").append(",");
+						bigSmall.append(Integer.parseInt(openNums[j]) >= 6 ? "3":"4").append(",");
+						dragonTiger.append(Integer.parseInt(openNums[j]) > Integer.parseInt(openNums[9-j])? "5":"6").append(",");
+					}
+					String bigSmallStr = bigSmall.substring(0, bigSmall.length()-1);
+					String oddEvenStr = oddEven.substring(0, oddEven.length()-1);
+					String dragonTigerStr = dragonTiger.substring(0, dragonTiger.length()-1);
+					lotteryHistory.setBigSmall(bigSmallStr);
+					lotteryHistory.setOddEven(oddEvenStr);
+					int oneTwoSum = Integer.parseInt(openNums[0])+Integer.parseInt(openNums[1]);
+					lotteryHistory.setOneTwoSum(oneTwoSum+","+(oneTwoSum % 2 == 0? "2": "1")+","+(oneTwoSum > 11 ? "3" : "4"));
+					lotteryHistory.setTragonTiger(dragonTigerStr);
+					list.add(lotteryHistory);
+				}
+				lotteryHistoryService.synchronizeLastDay(lastDay, list);
+			
+			} catch (Exception e) {
+				LOGGER.warn("DailyLotteryTask fialed",e);
+				LOGGER.warn("will try again....");
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					LOGGER.error("sleep thread interrupted:",e);
+				}
+				continue;
 			}
-			String bigSmallStr = bigSmall.substring(0, bigSmall.length()-1);
-			String oddEvenStr = oddEven.substring(0, oddEven.length()-1);
-			String dragonTigerStr = dragonTiger.substring(0, dragonTiger.length()-1);
-			lotteryHistory.setBigSmall(bigSmallStr);
-			lotteryHistory.setOddEven(oddEvenStr);
-			int oneTwoSum = Integer.parseInt(openNums[0])+Integer.parseInt(openNums[1]);
-			lotteryHistory.setOneTwoSum(oneTwoSum+","+(oneTwoSum % 2 == 0? "2": "1")+","+(oneTwoSum > 11 ? "3" : "4"));
-			lotteryHistory.setTragonTiger(dragonTigerStr);
-			list.add(lotteryHistory);
+			break;
 		}
-		lotteryHistoryService.synchronizeLastDay(lastDay, list);
+		long costTime = System.currentTimeMillis()-startTime;
+		String costTimeStr = costTime > 1000 ? costTime/1000.0+"秒":costTime+"毫秒";
+		LOGGER.info("DailyLotteryTask cost time:{}", costTimeStr);
+		LOGGER.info("DailyLotteryTask ends.....");
+		
 	}
 }
