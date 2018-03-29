d66464a1543478b38ec206e6623db2830c86218f
rejoiceFromJesus <948870341@qq.com>
2018-03-29 13:17:29 +0800

    11

--- src/main/java/com/pk10/active/console/controller/client/LotteryController.java
+++ src/main/java/com/pk10/active/console/controller/client/LotteryController.java
@@ -16,14 +16,18 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pk10.active.console.common.bean.Result;
+import com.pk10.active.console.entity.LotteryHistory;
 import com.pk10.active.console.service.CacheService;
+import com.pk10.active.console.service.LotteryHistoryService;
 import com.pk10.active.console.vo.CurrentPeriodLottery;
 
 
@@ -45,6 +49,9 @@ public class LotteryController {
 	@Autowired
 	CacheService cacheService;
 	
+	@Autowired
+	LotteryHistoryService lotteryHistoryService;
+	
 	@ApiOperation(value="最新一期开奖结果", notes="返回最新一期的开奖结果信息")
 	@GetMapping("/current-period")
 	public Result<CurrentPeriodLottery> currentPeriod(){
@@ -53,5 +60,13 @@ public class LotteryController {
 			currentPeriodLottery.setServerTime(new Date());
 		}
 		return Result.success(currentPeriodLottery);
+	}
+	@ApiOperation(value="开奖历史", notes="返回指定日期所有开奖历史")
+	@GetMapping("/history/{currentDay}")
+	public Result<List<LotteryHistory>> currentDayHistory(@PathVariable("currentDay") String currentDay){
+		LotteryHistory cons = new LotteryHistory();
+		cons.setOpenDate(currentDay);
+		List<LotteryHistory> list = lotteryHistoryService.queryListByWhereAndOrder(cons, new String[]{"period"}, new String[]{"desc"});
+		return Result.success(list);
 	}
 }
